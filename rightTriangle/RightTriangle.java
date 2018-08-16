8ca1851b34995eec4b2e80dad0aec060367e87b9
Abdul Bari <Bari@inadmin-5.local>
2018-08-16 11:38:52 +0530

    Add RightTriangle

---
+++ rightTriangle/RightTriangle.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class RightTriangle {
+  public void printAsterisk() {
+    System.out.print("*");
+  }
+
+  public void drawRightTriangle (int n) {
+    for(int j = n - 1; j >= 0; j--){
+      for(int i = 0; i < n - j; i++) {
+        printAsterisk();
+      }
+      System.out.println("");
+    }
+  }
+  public static void main(String[] args) {
+    System.out.print("Please enter a number: ");
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    RightTriangle vline = new RightTriangle();
+    vline.drawRightTriangle(n);
+  }
+}
